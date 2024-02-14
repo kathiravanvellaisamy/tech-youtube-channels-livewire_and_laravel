e24a47f2949ffc898b1cd3ddc4ded09a74fd2ca0
kathiravan vellaisamy <kathiravanvellaisamy@gmail.com>
2024-02-14 10:37:40 +0530

    text alignment in home page

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -1,8 +1,8 @@
 <div class="h-100 d-flex align-items center">
-    <div class="px-4 py-5 my-5 text-center">
+    <div class="px-4 py-3 my-5 text-center">
         <i class="bi bi-youtube" style="font-size: 80px; color:red" class="fw-bold fs-1 text-danger"></i>
         <h1 class="display-5 fw-bold text-body-emphasis">Tech Youtubers</h1>
-        <div class="col-lg-6 mx-auto">
+        <div class="col-lg-10 mx-auto">
             <p class="lead mb-4">Technology YouTube Channels Best List. Keep up with videos about New Technology,
                 Technology News, Latest Technology, Tech Updates, Tech Reviews, Tech Events, Tech Launch and many
                 more.No matter if you want to learn to code or get the inside scoop on the latest tech YouTubers are an
