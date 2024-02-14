7c8858e9d4f941f670f4393fa35befc880cf620a
kathiravan vellaisamy <kathiravanvellaisamy@gmail.com>
2024-02-14 10:22:43 +0530

    Created navbar with set hyperlinks

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -8,14 +8,16 @@
     <title>{{ $title ?? 'Tech Youtubers' }}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
 </head>
 
 <body style="font-family: 'Ubuntu', sans-serif !important; letter-spacing: 1px">
     <nav class="navbar navbar-dark  navbar-expand-lg bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="#">Navbar scroll</a>
+            <a wire:navigate class="navbar-brand fs-2 d-flex align-content-center flex-wrap gap-2" href="/">
+                <i class="bi bi-youtube"></i> <span>Youtubers</span>
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
                 aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -23,27 +25,18 @@
             <div class="collapse navbar-collapse" id="navbarText">
                 <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a wire:navigate class="nav-link active fs-5" aria-current="page" href="/">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            Link
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
+                        <a wire:navigate class="nav-link active fs-5" aria-current="page"
+                            href="/youtubers">Dashboard</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Link</a>
+                        <a wire:navigate class="nav-link active fs-5" aria-current="page" href="/youtubers/store">Add
+                            New</a>
+                    </li>
+                    <li class="nav-item">
+                        <a wire:navigate class="nav-link active fs-5" aria-current="page" href="/logout">Logout</a>
                     </li>
                 </ul>
                 <form class="d-flex">
