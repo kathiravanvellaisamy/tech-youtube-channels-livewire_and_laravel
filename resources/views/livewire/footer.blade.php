691642ab8128ec223a3e6a16f1a7a45beb3aeb72
kathiravan vellaisamy <kathiravanvellaisamy@gmail.com>
2024-02-14 22:05:41 +0530

    added footer component

---
+++ resources/views/livewire/footer.blade.php
@@ -0,0 +1,20 @@
+  <div class="container" style="margin-top: 40px">
+      <footer class="d-flex mt-20 flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+
+          <p class="col-md-4 mb-0 text-muted">&copy; 2024 Youtube, Inc</p>
+
+          <a wire:navigate href="/"
+              class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+              <i class="bi bi-youtube" style="font-size: 80px; color:red"></i>
+          </a>
+
+          <ul class="nav col-md-4 justify-content-end">
+              <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
+              <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
+              <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
+              <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
+              <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+          </ul>
+
+      </footer>
+  </div>
